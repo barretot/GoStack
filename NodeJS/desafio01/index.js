ab43130f09e6dfbc7092aa0101fc2a565cdb236b
Ruan Barreto <barretoruan77@gmail.com>
2020-01-30 14:38:57 -0300

    Desafio 1 finalizado!

--- NodeJS/desafio01/index.js
+++ NodeJS/desafio01/index.js
@@ -3,48 +3,75 @@ const express = require('express')
 const server = express()
 server.use(express.json())
 
-const projects = [{ id: 1, title: "Novo Projeto", tasks: ["Fazer compras", "Buscar as crianças"]}]
+const projects = [{ id: 1, title: "Novo Projeto", tasks: ["Fazer compras", "Buscar as crianças"] }]
 
-server.get('/projects', (req, res) => {
-   res.json(projects)
+function logRequests(req, res, next) {
+    console.count("Número de requisições");
+
+    return next();
+}
+
+server.use(logRequests)
+
+server.use((req, res, next) => {
+    console.time('Request')
+    console.log(`Method: ${req.method}; URL: ${req.url}`)
+
+    next()
+
+    console.timeEnd('Request')
 })
 
-server.get(`/projects/:id`, (req, res) => {
+
+function checkProjectExists(req, res, next) {
+    if (!projects[req.params.id - 1]) {
+        return res.status(400).json({ error: 'Project does not exists' })
+    }
+
+    return next();
+}
+
+
+server.get('/projects', (req, res) => {
+    res.json(projects)
+})
+
+server.get(`/projects/:id`, checkProjectExists, (req, res) => {
 
     const { id } = req.params
 
-    res.json( projects[id-1] );    
-  
+    res.json(projects[id - 1]);
+
 })
 
 server.post('/projects', (req, res) => {
-    const { id, title, tasks} = req.body
-    projects.push({id, title, tasks})
-    
-    return res.json(projects)
-})
-
-server.post('/projects/:id/tasks', (req, res) => {
-    const { id } = req.params
-    const { title } = req.body
-
-    projects[id-1].tasks.push(title)
+    const { id, title, tasks } = req.body
+    projects.push({ id, title, tasks })
 
     return res.json(projects)
 })
 
-server.put('/projects/:id', (req, res) => {
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
     const { id } = req.params
     const { title } = req.body
-  
-    projects[ id-1 ].title = title
-  
+
+    projects[id - 1].tasks.push(title)
+
+    return res.json(projects)
+})
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+    const { id } = req.params
+    const { title } = req.body
+
+    projects[id - 1].title = title
+
     return res.json([projects])
 })
 
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
     const { id } = req.params
-    projects.splice(id-1, 1)
+    projects.splice(id - 1, 1)
 
     return res.send()
 })
